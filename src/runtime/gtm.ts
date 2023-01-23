b33a389c7f701aa8bd6a5c3291a8054f77619db9
Emile Caron <zozorleponey@gmail.com>
2023-01-23 17:32:05 +0100

    gtm plugin

---
+++ src/runtime/gtm.ts
@@ -0,0 +1,19 @@
+
+// @ts-ignore
+import { defineNuxtPlugin, useRuntimeConfig, useHead } from "#app";
+
+export default defineNuxtPlugin(() => {
+  const gtmId = useRuntimeConfig()?.public.gtm?.id;
+
+  if (!gtmId) { return }
+
+  useHead({
+    script: [
+      { children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');` },
+    ],
+    noscript: [
+      { tagPosition: "bodyOpen", children: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}" height="0" width="0" style="display: none; visibility: hidden"></iframe>` }
+    ]
+  })
+
+})
