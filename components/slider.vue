df561f0b3a0491de1edda9ef77a27ec006fabff0
Emile Caron <zozorleponey@gmail.com>
2023-02-16 16:39:33 +0100

    refactored + fixed vertical scroller

--- components/slider.vue
+++ components/slider.vue
@@ -1,16 +1,67 @@
 
 <script lang="ts">
 export type ScrollDirection = "horizontal" | "vertical"
+
+export class ScrollController {
+    #el: HTMLElement;
+    #direction: ScrollDirection;
+
+    constructor(element: HTMLElement, direction: ScrollDirection) {
+        this.#el = element
+        this.#direction = direction
+    }
+
+    // current scroll position
+    get position(): number {
+        return this.#direction === "horizontal" ? this.#el.scrollLeft : this.#el.scrollTop
+    }
+
+    // scroll area size = front
+    get size(): number {
+        return this.#direction === "horizontal" ? this.#el.clientWidth : this.#el.clientHeight
+    }
+
+    // scroll area real size = back
+    get max(): number {
+        return this.#direction === "horizontal" ? this.#el.scrollWidth : this.#el.scrollHeight
+    }
+
+    reachedStart(): boolean {
+        return Math.round(this.position) <= 0
+    }
+
+    reachedEnd(): boolean {
+        return Math.round(this.position) >= this.max - this.size
+    }
+
+    scroll(value: number, behavior: ScrollBehavior) {
+        value = Math.round(value)
+        if (this.#direction === "horizontal") {
+            this.#el.scroll({ left: value, behavior })
+        } else {
+            this.#el.scroll({ top: value, behavior })
+        }
+    }
+
+    scrollBy(value: number, behavior: ScrollBehavior) {
+        value = Math.round(value)
+        if (this.#direction === "horizontal") {
+            this.#el.scrollBy({ left: value, behavior })
+        } else {
+            this.#el.scrollBy({ top: value, behavior })
+        }
+    }
+}
 </script>
 
 <script setup lang="ts">
 interface Props<T> {
     items: T[] | null
-    scrollBehavior: ScrollBehavior
+    scrollBehavior?: ScrollBehavior
     direction?: ScrollDirection
     class?: string[]
     scrollerClass?: string[]
-    autoscroll?: boolean 
+    autoscroll?: boolean
 }
 
 const props = withDefaults(defineProps<Props<any>>(), {
@@ -21,60 +72,47 @@ const props = withDefaults(defineProps<Props<any>>(), {
     autoscroll: false,
 })
 
+let scrollController: ScrollController
+const hover = ref(false)
 const scrollerEl = ref<HTMLElement>()
 
 const previous = () => {
-    if (scrollerEl.value) {
-        const scrollPos = props.direction === "horizontal" ? scrollerEl.value.scrollLeft : scrollerEl.value.scrollTop
-        const scrollSize = props.direction === "horizontal" ? scrollerEl.value.clientWidth : scrollerEl.value.clientHeight
-        const scrollMaxSize = props.direction === "horizontal" ? scrollerEl.value.scrollWidth : scrollerEl.value.scrollHeight
-
-        if (Math.round(scrollPos) <= 0) {
-            scrollerEl.value.scroll({
-                left: scrollMaxSize - scrollSize,
-                behavior: props.scrollBehavior
-            })
+    if (scrollController) {
+        if (scrollController.reachedStart()) {
+            scrollController.scroll(scrollController.max - scrollController.size, props.scrollBehavior)
         } else {
-            scrollerEl.value.scrollBy({
-                left: -Math.round(0.8 * scrollSize),
-                behavior: props.scrollBehavior,
-            })
+            scrollController.scrollBy(-0.8 * scrollController.size, props.scrollBehavior)
         }
     }
 }
 
 const next = () => {
-    if (scrollerEl.value) {
-        const scrollPos = props.direction === "horizontal" ? scrollerEl.value.scrollLeft : scrollerEl.value.scrollTop
-        const scrollSize = props.direction === "horizontal" ? scrollerEl.value.clientWidth : scrollerEl.value.clientHeight
-        const scrollMaxSize = props.direction === "horizontal" ? scrollerEl.value.scrollWidth : scrollerEl.value.scrollHeight
-
-        if (Math.round(scrollPos) >= scrollMaxSize - scrollSize) {
-            scrollerEl.value.scroll({
-                left: 0,
-                behavior: props.scrollBehavior
-            })
+    if (scrollController) {
+        if (scrollController.reachedEnd()) {
+            scrollController.scroll(0, props.scrollBehavior)
         } else {
-            scrollerEl.value.scrollBy({
-                left: Math.round(0.8 * scrollSize),
-                behavior: props.scrollBehavior,
-            })
+            scrollController.scrollBy(0.8 * scrollController.size, props.scrollBehavior)
         }
     }
 }
 
-const hover = ref(false)
 if (props.autoscroll) {
     window.setInterval(() => {
-        if (scrollerEl.value && !hover.value) {
-            if (props.direction === "horizontal") {
-                scrollerEl.value.scrollBy(1, 0)
+        if (scrollController && !hover.value) {
+            if (scrollController.reachedEnd()) {
+                scrollController.scroll(0, "auto")
             } else {
-                scrollerEl.value.scrollBy(0, 1)
+                scrollController.scrollBy(1, "auto")
             }
         }
     }, 10)
 }
+
+onMounted(() => {
+    if (scrollerEl.value) {
+        scrollController = new ScrollController(scrollerEl.value, props.direction)
+    }
+})
 
 const mainClass = [
     "relative",
@@ -96,8 +134,7 @@ const previousClass = [
     "hidden",
     "md:block",
     "top-0",
-    //"left-0",
-    "right-0",
+    "left-0",
     props.direction === "horizontal" ? "bottom-0" : "right-0",
 ]
 
@@ -106,8 +143,7 @@ const nextClass = [
     "hidden",
     "md:block",
     "bottom-0",
-    //"right-0",
-    ""
+    "right-0",
     props.direction === "horizontal" ? "top-0" : "left-0",
 ]
 
@@ -123,21 +159,23 @@ const nextContentClass = previousContentClass.concat()
 </script>
 
 <template>
-    <div :class="mainClass" v-if="props.items">
-        <div :class="scrollerClass" ref="scrollerEl" @mouseenter="hover=true" @mouseleave="hover=false">
+    <div :class="mainClass" v-if="props.items" @mouseenter="hover = true" @mouseleave="hover = false">
+        <div :class="scrollerClass" ref="scrollerEl">
             <slot name="item" v-for="item in props.items" :item="item"> </slot>
         </div>
         <div :class="previousClass">
             <div :class="previousContentClass">
                 <slot name="previous-btn" :click="previous">
-                    <div @click="previous" class="hover:bg-slate-500/10 cursor-pointer rounded-full px-4 text-3xl">&lt;</div>
+                    <div @click="previous" class="hover:bg-slate-500/10 cursor-pointer rounded-full px-4 text-3xl">&lt;
+                    </div>
                 </slot>
             </div>
         </div>
         <div :class="nextClass">
             <div :class="nextContentClass">
                 <slot name="next-btn" :click="next">
-                    <div @click="next" class="hover:bg-slate-500/10 cursor-pointer rounded-full px-4 text-3xl">&gt;</div>
+                    <div @click="next" class="hover:bg-slate-500/10 cursor-pointer rounded-full px-4 text-3xl">&gt;
+                    </div>
                 </slot>
             </div>
         </div>
