a7a382e56619ba2dca54056f7e522383655a58f0
Emile Caron <zozorleponey@gmail.com>
2023-02-20 18:00:35 +0100

    wip

---
+++ components/search.vue
@@ -0,0 +1,34 @@
+
+<script setup lang="ts">
+const { $isSafeLink } = useNuxtApp()
+
+interface Props {
+    value: string,
+    redirectUrl: string,
+    redirectSearchParam?: string,
+}
+
+
+const props = withDefaults(defineProps<Props>(), {
+    redirectSearchParam: "q",
+})
+
+// parse url at load time to make sure its valid
+new URL(props.redirectUrl)
+
+// on submit, redirect to mirrored domain search url
+const submit = () => {
+    if (props.value) {
+        let url = new URL(props.redirectUrl)
+        url.searchParams.set(props.redirectSearchParam, props.value)
+        window.location.href = url.toString()
+    }
+}
+
+</script>
+
+<template>
+    <form @submit="submit">
+        <slot></slot>
+    </form>
+</template>
