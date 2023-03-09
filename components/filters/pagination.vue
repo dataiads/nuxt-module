2d1b7b4f2a731f74099311c4833825796637fd4a
Robin Christol <robin@dataiads.io>
2023-03-09 10:42:37 +0100

    pagination option

--- components/filters/pagination.vue
+++ components/filters/pagination.vue
@@ -2,18 +2,19 @@
 <script setup lang="ts">
 
 interface Props {
-    filter: Filter
-    maxVisibleButtons?: number
-    class?: string | string[]
+  filter: Filter
+  maxVisibleButtons?: number
+  class?: string | string[]
+  action?: 'one-by-one' | 'first-last'
 }
 
 const props = withDefaults(defineProps<Props>(), {
-    maxVisibleButtons: 4,
-    class: "flex items-center",
+  maxVisibleButtons: 4,
+  class: "flex items-center",
+  action: 'first-last',
 })
 
 let totalResults = props.filter.count
-
 const pageCount = computed(() => Math.ceil(totalResults.value / props.filter.limit.value))
 
 
@@ -21,34 +22,60 @@ const minVisiblePage = computed(() => Math.max(1, Math.ceil(props.filter.page.va
 const maxVisiblePage = computed(() => Math.min(pageCount.value, minVisiblePage.value + props.maxVisibleButtons - 1))
 
 const range = (min: number, max: number): number[] => {
-    let items: number[] = []
-    for (let i=min; i <= Math.max(min, max); i++) {
-        items.push(i)
-    }
-    return items
+  let items: number[] = []
+  for (let i = min; i <= Math.max(min, max); i++) {
+    items.push(i)
+  }
+  return items
 }
+
+let actionHandlerLeft = () => {};
+let actionHandlerRight = () => {};
+
+if (props.action === 'one-by-one') {
+  actionHandlerLeft = () => {
+    if (props.filter.page.value > 1) {
+      props.filter.page.value -= 1;
+    }
+  }
+  actionHandlerRight = () => {
+    if (props.filter.page.value < pageCount.value) {
+      props.filter.page.value += 1;
+    }
+  }
+}
+
+if (props.action === 'first-last') {
+  actionHandlerLeft = () => {
+    props.filter.page.value = 1;
+  }
+  actionHandlerRight = () => {
+    props.filter.page.value = pageCount.value;
+  }
+}
+
 </script>
 
 <template>
-    <nav :class="props.class">
-        <div v-if="props.filter.page.value > 1" @click="props.filter.page.value =  1" alt="go to previous page">
-            <slot name="previous-button">
-                <button type="button">&lt;</button>
-            </slot>
-        </div>
+  <nav :class="props.class">
+    <div v-if="props.filter.page.value > 1" @click="actionHandlerLeft" alt="go to previous page">
+      <slot name="previous-button">
+        <button type="button">&lt;</button>
+      </slot>
+    </div>
 
-        <template v-for="index in range(minVisiblePage, maxVisiblePage)" :key="index">
-            <div  @click="props.filter.page.value = index"  :alt="`go to page ${index}`">
-                <slot name="page-button" :number="index" :active="index == props.filter.page.value">
-                    <button type="button">{{ index }}</button>
-                </slot>
-            </div>
-        </template>
+    <template v-for="index in range(minVisiblePage, maxVisiblePage)" :key="index">
+      <div @click="props.filter.page.value = index" :alt="`go to page ${index}`">
+        <slot name="page-button" :number="index" :active="index == props.filter.page.value">
+          <button type="button">{{ index }}</button>
+        </slot>
+      </div>
+    </template>
 
-        <div v-if="props.filter.page.value < pageCount" @click="props.filter.page.value = pageCount" alt="go to next page">
-            <slot name="next-button">
-                <button type="button">&gt;</button>
-            </slot>
-        </div>
-    </nav>
+    <div v-if="props.filter.page.value < pageCount" @click="actionHandlerRight" alt="go to next page">
+      <slot name="next-button">
+        <button type="button">&gt;</button>
+      </slot>
+    </div>
+  </nav>
 </template>
