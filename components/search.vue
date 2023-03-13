888e1252f1d2e012319fb0ae9ea621a93b75e7fd
Hugo Moracchini <hugo4400@gmail.com>
2023-03-13 15:42:08 +0100

    Don't use `SEARCH` operator, add default item display

--- components/search.vue
+++ components/search.vue
@@ -55,8 +55,8 @@ if (props.lpoSearchReccomendations) {
             [
                 {
                     criteria: "title",
-                    operator: "SEARCH",
-                    value: searchValue.value,
+                    operator: "CONTAINS_ALL_CI",
+                    value: searchValue.value.split(" ").join(","),
                 }
             ]
         ])
@@ -100,13 +100,47 @@ const input = (event: Event) => {
             <slot name="search-slider-header"></slot>
             <Slider :items="searchRecoProducts" :autoscroll="false" :direction="direction">
                 <template #item="{ item }">
-                    <slot name="search-slider-item" :key="item.id" :item="item"></slot>
+                    <slot name="search-slider-item" :key="item.id" :item="item">
+                        <div class="bg-white flex content-center max-w-[540px] p-2.5">
+                            <div class="flex">
+                                <div class="pr-1 h-[120px]">
+                                    <figure class="relative max-w-sm cursor-pointer">
+                                        <a aria-label="view search result" :href="$oriUrl(item.data.link)">
+                                            <img :src="item.data.imageLink" width="120" height="120" class="mx-auto h-[120px] w-auto" loading="lazy"
+                                            alt="recommended product image">
+                                        </a>
+                                    </figure>
+                                </div>
+                                <div class="flex flex-col flex-1 py-3 pl-2">
+                                    <a :href="$oriUrl(item.data.link)" class="uppercase font-bold font-custom line-clamp-2 leading-[19px]">{{
+                                    item?.extraData?.title ||
+                                    item.data.title }}</a>
+
+                                    <div class="flex items-center">
+                                        <PriceDisplay :product="item">
+                                            <template #price="{ price }">
+                                                <span class="text-[15px] font-medium">{{ price }}&euro;</span>
+                                            </template>
+                                            <template #sale-price="{ price, salePrice }">
+                                                <span class="text-[15px] font-medium">{{ salePrice }}&euro;</span>
+                                                <div class="line-through text-gray-400 text-right ml-1.5 text-[13px] leading-[14px]">{{ price }}&euro;</div>
+                                            </template>
+                                        </PriceDisplay>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </slot>
                 </template>
                 <template #previous-btn="scope">
-                    <slot name="search-slider-previous-btn" v-bind="scope"></slot>
+                    <slot name="search-slider-previous-btn" v-bind="scope">
+                        <div></div>
+                    </slot>
                 </template>
                 <template #next-btn="scope">
-                    <slot name="search-slider-next-btn" v-bind="scope"></slot>
+                    <slot name="search-slider-next-btn" v-bind="scope">
+                        <div></div>
+                    </slot>
                 </template>
             </Slider>
         </div>
