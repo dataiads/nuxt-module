f1374dbd38d5338227d1b9e377eb9edce8f0d5b9
Emile Caron <zozorleponey@gmail.com>
2023-03-23 09:54:49 +0100

    proper typing

--- components/image.vue
+++ components/image.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
-const props = defineProps<{
+
+interface Props {
 	src: string,
 	width: string,
 	height: string,
@@ -8,28 +9,33 @@ const props = defineProps<{
 	format?: string,
 	alt?: string,
 	picture?: boolean
-}>()
+	zoomable?: boolean
+}
 
-const format = props.format ?? "webp"
-const loading = props.loading ?? "eager"
-const picture = props.picture ?? false;
+const props = withDefaults(defineProps<Props>(), {
+	format: "webp",	
+	loading: "eager",
+	picture: false,
+	zoomable: false,
+})
 
 const fallbackToUncompressed = (event: Event) => {
-	if (event.target.src != props.src) {
-		event.target.src = props.src
+	const target = event.target as HTMLImageElement
+	if (target.src != props.src) {
+		target.src = props.src
 	}
 }
 
 </script>
 
 <template>
-	<nuxt-img v-if="config.public.optimizeImageLoad && !picture" :src="props.src" :alt="props.alt"
-		@error="fallbackToUncompressed" :width="props.width" :height="props.height" :format="format"
+	<nuxt-img v-if="config.public.optimizeImageLoad && !props.picture" :src="props.src" :alt="props.alt"
+		@error="fallbackToUncompressed" :width="props.width" :height="props.height" :format="props.format"
 		:loading="loading" />
-	<picture v-else-if="config.public.optimizeImageLoad && picture">
+	<picture v-else-if="config.public.optimizeImageLoad && props.picture">
 		<slot></slot>
 		<nuxt-img :src="props.src" :alt="props.alt" @error="fallbackToUncompressed" :width="props.width"
-			:height="props.height" :format="format" :loading="loading" />
+			:height="props.height" :format="props.format" :loading="props.loading" />
 	</picture>
-	<img v-else :src="props.src" :alt="props.alt" :width="props.width" :height="props.height" :loading="loading">
+	<img v-else :src="props.src" :alt="props.alt" :width="props.width" :height="props.height" :loading="props.loading">
 </template>
