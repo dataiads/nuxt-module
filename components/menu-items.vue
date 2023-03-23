0ef83e7c5e263f567f4fb1cd324b139371cc9471
Emile Caron <zozorleponey@gmail.com>
2023-03-23 15:11:57 +0100

    default slot for menu item

--- components/menu-items.vue
+++ components/menu-items.vue
@@ -15,11 +15,15 @@ let entries = lpoConfig[props.configKey] || []
 <template>
     <template v-for="(item, index) in entries">
         <a v-if="item.href" :href="item.href" :style="{ color: item.color || 'inherit'}">
-            <slot :text="item.text"></slot>
+            <slot :text="item.text">
+                <li>{{ item.text }}</li>
+            </slot>
         </a>
         <span v-else :style="{ color: item.color || 'inherit'}">
 
-            <slot :text="item.text"></slot>
+            <slot :text="item.text">
+                <li>{{ item.text }}</li>
+            </slot>
         </span>
     </template>
 
