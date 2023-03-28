3402dbd8abee6e5a2ffada1f0e4c45ec11b8b467
Emile Caron <zozorleponey@gmail.com>
2023-03-28 15:58:25 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -64,7 +64,7 @@ export default defineNuxtConfig({
         mainProduct: {
           class: "w-full lg:container mx-auto flex flex-wrap lg:flex-nowrap lg:max-h-[80vh]",
           imageClass: "w-full lg:w-[400px] xl:w-[540px] flex-nowrap",
-          descriptionClass: " w-full lg:grow flex items-start md:my-auto",
+          descriptionClass: "w-full lg:grow flex items-start md:my-auto",
           verticalSliderClass: "hidden lg:block lg:w-[180px]",
         },
         recoSlider: {
