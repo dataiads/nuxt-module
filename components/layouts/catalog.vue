b4c4d4b4079aa03892f7934e4a730c180863d0cc
Emile Caron <zozorleponey@gmail.com>
2023-04-18 09:06:13 +0200

    main product aside slot

--- components/layouts/catalog.vue
+++ components/layouts/catalog.vue
@@ -26,6 +26,8 @@ const s = config.public.layoutStyle
 
         <div id="main-product" :class="s.mainProduct.class">
             <slot name="main-product">
+                <slot name="main-product-aside"></slot>
+                
                 <div :class="s.mainProduct.imageClass">
                     <slot name="main-product-image"></slot>
                 </div>
