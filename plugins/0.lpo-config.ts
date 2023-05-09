10adb6ab86b4333f66404ef284cc5f3e0ca0a810
Hugo Moracchini <hugo4400@gmail.com>
2023-05-09 16:51:50 +0200

    Implement extraReco lol

--- plugins/0.lpo-config.ts
+++ plugins/0.lpo-config.ts
@@ -62,6 +62,7 @@ export default defineNuxtPlugin(() => {
             useLightMainProduct: StringLoader,
             customerSpecific: JSONLoader,
             cssVariables: JSONLoader,
+            extraReco: StringLoader,
         }
 
 
