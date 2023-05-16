9eaa00c5f4d63f39f81e7f78518c55a4c5df5479
Emile Caron <emile@dataiads.io>
2023-05-16 14:49:45 +0200

    optional hover src for images

--- components/image.vue
+++ components/image.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
+
 const props = defineProps<{
 	src: string,
+	hoverSrc?: string,
 	width: string,
 	height: string,
 	loading?: string,
@@ -24,16 +26,37 @@ const fallbackToUncompressed = (event: Event) => {
 	}
 }
 
+const hover = ref(false)
+const src = computed(() => {
+	if (hover.value && props.hoverSrc) {
+		return props.hoverSrc
+	}
+	return props.src
+})
+
+
 </script>
 
 <template>
-	<nuxt-img v-if="config.public.optimizeImageLoad && !picture" :src="props.src" :alt="props.alt"
-		@error="fallbackToUncompressed" :width="props.width" :height="props.height" :format="format"
-		:loading="loading" />
-	<picture v-else-if="config.public.optimizeImageLoad && picture">
+	<nuxt-img
+		v-if="config.public.optimizeImageLoad && !picture"
+		:src="src" :alt="props.alt" :width="props.width" :height="props.height" :format="format" :loading="loading" 
+		@error="fallbackToUncompressed" 
+		@mouseover="hover = true"
+		@mouseleave="hover = false"
+	/>
+	<picture
+		v-else-if="config.public.optimizeImageLoad && picture">
 		<slot></slot>
-		<nuxt-img :src="props.src" :alt="props.alt" @error="fallbackToUncompressed" :width="props.width"
-			:height="props.height" :format="format" :loading="loading" />
+		<nuxt-img :src="src" :alt="props.alt" :width="props.width" :height="props.height" :format="format" :loading="loading"
+			@error="fallbackToUncompressed"
+			@mouseover="hover = true"
+			@mouseleave="hover = false"
+		/>
 	</picture>
-	<img v-else :src="props.src" :alt="props.alt" :width="props.width" :height="props.height" :loading="loading">
+	<img v-else
+		:src="src" :alt="props.alt" :width="props.width" :height="props.height" :loading="loading"
+		@mouseover="hover = true"
+		@mouseleave="hover = false"
+	>
 </template>
