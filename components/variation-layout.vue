9090548992b4f64b5a4d7d3bc74cf5ec5b67323c
Robin Christol <robin@dataiads.io>
2023-07-31 17:09:18 +0200

    new variation catalog-xsell-top-no-slider

--- components/variation-layout.vue
+++ components/variation-layout.vue
@@ -9,7 +9,7 @@ const props = defineProps<{
 </script>
 
 <script lang="ts">
-export type Variations = "catalog" | "catalog-high-filters" | "catalog-high-filters-no-slider" | "catalog-high-filters-top-slider" | "catalog-reco-overlay" | "catalog-custom-overlay-right" | "catalog-reco-overlay-right" | "catalog-no-slider" | "catalog-no-slider-xsell-top" | "catalog-top-slider" | "catalog-vertical-slider" | "light" | "light-no-slider" | "light-no-slider-xsell-top" | "light-top-slider" | "light-vertical-slider" | "catalog-xsell-top" | "light-xsell-top";
+export type Variations = "catalog" | "catalog-high-filters" | "catalog-high-filters-no-slider" | "catalog-high-filters-top-slider" | "catalog-reco-overlay" | "catalog-custom-overlay-right" | "catalog-reco-overlay-right" | "catalog-no-slider" | "catalog-no-slider-xsell-top" | "catalog-top-slider" | "catalog-vertical-slider" | "light" | "light-no-slider" | "light-no-slider-xsell-top" | "light-top-slider" | "light-vertical-slider" | "catalog-xsell-top" | "light-xsell-top" | "catalog-xsell-top-no-slider";
 </script>
 
 <template>
@@ -118,6 +118,12 @@ export type Variations = "catalog" | "catalog-high-filters" | "catalog-high-filt
             <slot :name="name" v-bind="scope"></slot>
         </template>
     </LayoutsCatalogXsellTop>
+
+    <LayoutsCatalogXsellTopNoSlider v-else-if="lpoConfig.variation === 'catalog-xsell-top-no-slider'" v-bind="props" >
+        <template v-for="(_, name) in $slots" #[name]="scope">
+            <slot :name="name" v-bind="scope"></slot>
+        </template>
+    </LayoutsCatalogXsellTopNoSlider>
 
     <!-- "catalog" is the default layout. -->
     <LayoutsCatalog v-else v-bind="props" >
