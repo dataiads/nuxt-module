a7474694e07e9f8576d5204af151153dcf7da537
sMesle <45283764+sMesle@users.noreply.github.com>
2023-11-16 13:54:43 +0100

    New components to handle filter from config (#41)
    * new components to handle filter from config
    * add slot checkbox to `autolist-checkbox` and `checkbox`
    * add `container` and `elements` components to handle filter from lpoConfig
    
    * checkbox : fix name slot scoped slot for label

--- components/filters/checkbox.vue
+++ components/filters/checkbox.vue
@@ -2,49 +2,94 @@
 // random id to link label to the input
 const uid = Math.floor(Math.random() * 10 ** 16).toString();
 
-const props = withDefaults(defineProps<{
-  label?: string
-  filter: Recommender
-  criteria: string
-  operator?: string
-  value?: string
-  valueCriteria?: string
-  baseProductCriteria?: string
-  group: string
-  class?: string
-  inputClass?: string
-  inputStyle?: string|Record<string,any>
-  labelClass?: string
-}>(), {
-  label: "",
-  operator: "EQUAL",
-  value: "",
-  valueCriteria: "",
-  baseProductCriteria: "",
-  class: "flex items-center gap-1 my-1",
-  inputClass: "hover:cursor-pointer",
-  labelClass: "",
-})
+const props = withDefaults(
+  defineProps<{
+    label?: string;
+    filter: Recommender;
+    criteria: string;
+    operator?: string;
+    value?: string;
+    valueCriteria?: string;
+    baseProductCriteria?: string;
+    group: string;
+    class?: string;
+    inputClass?: string;
+    inputStyle?: string | Record<string, any>;
+    labelClass?: string;
+  }>(),
+  {
+    label: "",
+    operator: "EQUAL",
+    value: "",
+    valueCriteria: "",
+    baseProductCriteria: "",
+    class: "flex items-center gap-1 my-1",
+    inputClass: "hover:cursor-pointer",
+    labelClass: "",
+  }
+);
+
+const get = () => {
+  return props.filter.hasRule(
+    props.group,
+    props.criteria,
+    props.operator,
+    props.value,
+    props.valueCriteria,
+    props.baseProductCriteria
+  );
+};
+
+const set = (v) => {
+  if (v) {
+    props.filter.pushRule(
+      props.group,
+      props.criteria,
+      props.operator,
+      props.value,
+      props.valueCriteria,
+      props.baseProductCriteria
+    );
+  } else {
+    props.filter.removeRule(
+      props.group,
+      props.criteria,
+      props.operator,
+      props.value,
+      props.valueCriteria,
+      props.baseProductCriteria
+    );
+  }
+};
 
 let binder = computed({
-  get: () =>
-    props.filter.hasRule(props.group, props.criteria, props.operator, props.value, props.valueCriteria, props.baseProductCriteria),
+  get: () => get(),
   set: (val) => {
-    if (val) {
-      props.filter.pushRule(props.group, props.criteria, props.operator, props.value, props.valueCriteria, props.baseProductCriteria);
-    } else {
-      props.filter.removeRule(props.group, props.criteria, props.operator, props.value, props.valueCriteria, props.baseProductCriteria);
-    }
+    set(val);
   },
 });
-
 </script>
 
 <template>
-<div :class="props.class">
-	<input type="checkbox" :id="uid" :class="props.inputClass" v-model="binder" :style="props.inputStyle" >
-	<label :class="props.labelClass" :for="uid" >
-    <slot name="label" :label="props.label" :checked="binder">{{ props.label }}</slot>
-  </label>
-</div>
+  <div :class="props.class">
+    <slot
+      name="checkbox"
+      :info="{ id: uid, type: 'checkbox' }"
+      :get="get"
+      :set="set"
+    >
+      <input
+        type="checkbox"
+        :id="uid"
+        :class="props.inputClass"
+        v-model="binder"
+        :style="props.inputStyle"
+      />
+    </slot>
+    <label :class="props.labelClass" :for="uid">
+      <slot name="label" :value="props.label" :checked="binder">{{
+        props.label
+      }}</slot>
+    </label>
+  </div>
 </template>
---
+++ components/filters/container.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+const AsideItem = resolveComponent("AsideItem");
+
+const props = withDefaults(
+  defineProps<{
+    filter: Recommender;
+    open: boolean;
+    headerClass?: string;
+    asideItemClass?: string;
+    rootComponent?: Component;
+  }>(),
+  {
+    asideItemClass: "!h-auto px-4 py-2",
+    headerClass: "cursor-pointer lg:py-4 flex items-center",
+    rangeInputClass: "grid grid-cols-2 gap-2",
+    inputClass: "border-[#9E9E9E]",
+  }
+);
+
+const lpoConfig = useLpoConfig();
+const filterParams = lpoConfig?.filterParams;
+
+// Add the group key to props
+filterParams.forEach((f, i) => {
+  f.elements.forEach((el) => {
+    el.props.group = `${f.title}-${el.component}-filter`;
+  });
+});
+
+// Return the group from filterParam
+const getGroups = (filterParam) => {
+  return filterParam.elements.map((el) => el.props.group);
+};
+
+const removeAllRulesFromGroups = (groups: string[]) => {
+  groups.forEach((g) => props.filter.removeAllRules(g));
+};
+
+</script>
+
+<template>
+  <div>
+    <component
+      :is="rootComponent ? rootComponent : AsideItem"
+      :display="open"
+      :class="asideItemClass"
+      v-for="filterParam in filterParams"
+      :key="filterParam.title"
+      contentClass=""
+    >
+      <template #header="{ displayed }">
+        <slot name="header" :displayed="displayed" :title="filterParam.title">
+          <div :class="headerClass" v-bind:class="{ '': displayed }">
+            <slot name="title" :value="filterParam.title">
+              <span
+                class="grow whitespace-nowrap py-2 md:p-[0] font-bold md:text-[16px]"
+                >{{ filterParam.title }}</span
+              >
+            </slot>
+            <slot name="append" :displayed="displayed" />
+          </div>
+        </slot>
+      </template>
+
+      <template #content>
+        <slot
+          name="prepend-content"
+          :removeAllFilter="removeAllRulesFromGroups"
+          :groups="getGroups(filterParam)"
+        >
+        </slot>
+        <slot
+          name="content"
+          :elements="filterParam.elements"
+          :parameters="filterParam"
+          :removeAllFilter="removeAllRulesFromGroups(getGroups(filterParam))"
+        />
+      </template>
+    </component>
+  </div>
+</template>
