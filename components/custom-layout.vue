be91d4d3c9f379de18167e7da1dd02b52bd73e8b
smesle <steve.demeulemeester@efrei.net>
2023-11-22 09:00:45 +0100

    wip: custom layout

---
+++ components/custom-layout.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps(layoutProps);
+
+const config = {
+    prependHeader: [
+      {
+        type: "banner",
+        options: {},
+      },
+    ],
+    postHeader:[
+      {
+        type: "breadcrumb",
+        options: {
+          enable: true,
+        },
+      },
+    ]
+}
+</script>
+
+<template>
+  <div class="lg:mx-auto">
+    <!-- Prepend Header-->
+    <div>
+      <slot name="sticky-reco-overlay" />
+    </div>
+
+    <header id="header">
+      <slot name="header" />
+    </header>
+
+    <div v-for="(element, i) in config.postHeader" :id="element.type" :key="'postheader_' + i" >
+      <slot v-if="element.options.enable" :name="element.type">
+    </slot>
+    </div>
+
+    <main id="main-product"></main>
+  </div>
+</template>
