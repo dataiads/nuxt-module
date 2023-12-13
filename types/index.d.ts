b727c75c1f91bc301e51cc7f5fdb9b13f490bb90
Emile Caron <emile@dataiads.io>
2023-12-13 09:56:22 +0100

    update typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -319,8 +319,8 @@ declare global {
   export interface RecommenderResults {
     data: ComputedRef<any | null>;
     pending: Ref<boolean>;
-    refresh: (opts?: any) => Promise<Product[] | null>;
-    execute: (opts?: any) => Promise<Product[] | null>;
+    refresh: (opts?: any) => Promise<void>;
+    execute: (opts?: any) => Promise<void>;
     error: Ref<FetchError<any> | null>;
   }
 
