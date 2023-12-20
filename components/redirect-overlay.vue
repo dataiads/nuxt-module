e2ddbf79d6fd605b2b48da04a1a5a60db774b37e
Hugo Moracchini <hugo4400@gmail.com>
2023-12-20 11:03:44 +0100

    tailwind spin

--- components/redirect-overlay.vue
+++ components/redirect-overlay.vue
@@ -16,7 +16,7 @@ const message = useState<string>("redirect.message", () => "");
         <slot name="overlay">
             <div class="h-full w-full bg-grey1 bg-opacity-80 flex flex-col items-center justify-center">
                 <slot name="loader">
-                    <div class="mx-auto loader ease-linear rounded-full border-4 border-t-4 border-t-primary border-gray-200 h-12 w-12 mb-4"></div>
+                    <div class="mx-auto animate-spin ease-linear rounded-full border-4 border-t-4 border-t-primary border-gray-200 h-12 w-12 mb-4"></div>
                 </slot>
                 <slot name="message">
                     <h2 v-if="message" class="text-center text-xl font-semibold">{{ message }}</h2>
@@ -26,28 +26,3 @@ const message = useState<string>("redirect.message", () => "");
     </div>
 </transition>
 </template>
-
-<style scoped>
-.loader {
-	-webkit-animation: spinner 1.5s linear infinite;
-	animation: spinner 1.5s linear infinite;
-}
-
-@-webkit-keyframes spinner {
-	0% {
-		-webkit-transform: rotate(0deg);
-	}
-	100% {
-		-webkit-transform: rotate(360deg);
-	}
-}
-
-@keyframes spinner {
-	0% {
-		transform: rotate(0deg);
-	}
-	100% {
-		transform: rotate(360deg);
-	}
-}
-</style>
