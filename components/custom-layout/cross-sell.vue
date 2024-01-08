ea9d73f8c3820d683235f5e70c9bbc229d0d556a
smesle <steve.demeulemeester@efrei.net>
2024-01-08 17:24:44 +0100

    fix(CustomLayout): Slider use only config button

--- components/custom-layout/cross-sell.vue
+++ components/custom-layout/cross-sell.vue
@@ -57,7 +57,9 @@ if (props.config.data) {
       </template>
       <template #previous-btn="scope">
         <template v-if="config.previousButton">
-          <img :src="config.previousButton" class="cursor-pointer" :style="config.buttonStyle" @click="scope.click">
+          <button @click="scope.click">
+            <img :style="config.buttonStyle" :src="config.previousButton">
+          </button>
         </template>
         <template v-else>
           <slot name="cross-sell-previous-btn" v-bind="scope" />
@@ -65,10 +67,9 @@ if (props.config.data) {
       </template>
       <template #next-btn="scope">
         <template v-if="config.nextButton">
-          <img :src="config.nextButton" class="cursor-pointer" :style="config.buttonStyle" @click="scope.click">
-        </template>
-        <template v-else>
-          <slot name="cross-sell-next-btn" v-bind="scope" />
+          <button @click="scope.click">
+            <img :style="config.buttonStyle" :src="config.nextButton">
+          </button>
         </template>
       </template>
     </Slider>
