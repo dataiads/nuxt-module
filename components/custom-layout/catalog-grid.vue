6cdf70e4ff5d94f4256f0c7a08c4ea7e9e3a355b
Emile Caron <emile@dataiads.io>
2024-01-11 14:14:07 +0100

    infinite scroll mode for main reco

---
+++ components/custom-layout/catalog-grid.vue
@@ -0,0 +1,43 @@
+
+
+<script setup lang="ts">
+import type { MainRecoParams } from '~/types';
+import { vIntersectionObserver } from '@vueuse/components';
+
+defineProps<{
+    config: MainRecoParams;
+}>()
+
+const customLayout = useCustomLayout()
+
+const { y: yScroll } = useWindowScroll()
+
+const onIntersect = () => {
+    if (
+        customLayout &&
+        // user scrolled
+        yScroll.value > 0 &&
+        // initial recommendations loaded
+        customLayout.filter.count.value > 0 &&
+        // limit still under total count
+        customLayout.filter.limit.value < customLayout.filter.count.value
+    ) {
+        console.log("loading more")
+        customLayout.filter.loadMore()
+    }
+}
+
+</script>
+
+
+<template>
+    <div v-if="customLayout?.filter.results?.data?.value?.length" :style="config.gridStyle" id="catalog-grid">
+        <template v-for="(item, index) in customLayout.filter.results.data.value"
+            :key="item.id ? item.id : JSON.stringify(item)">
+            <slot name="filters-content-grid-item" :item="item" :index="index" />
+        </template>
+    </div>
+    <slot v-else name="filters-no-results" />
+    <!-- intersection observer for infinite scroll pagination type -->
+    <div v-intersection-observer="onIntersect" v-if="config.paginationType === 'scroll'"></div>
+</template>
