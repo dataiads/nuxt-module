73890b9f6e60c17f5f0f4387a92eedfcc95d8689
Ltsrc <42251639+koi-tattoo@users.noreply.github.com>
2024-01-17 15:59:03 +0100

    Cross sell key regex

--- components/custom-layout/cross-sell.vue
+++ components/custom-layout/cross-sell.vue
@@ -9,7 +9,15 @@ const props = defineProps<{
 const product = useProduct()
 
 // only one algorithm for now
-const keyMatcher = (productKey: string, dataKey: string) => productKey.startsWith(dataKey)
+const keyMatcher = (productKey: string, dataKey: string, regex?: string) => {
+  if (regex) {
+    const regexProductKey = productKey.match(regex)?.[0];
+    if (regexProductKey) {
+      return regexProductKey.startsWith(dataKey)
+    }
+  }
+  return productKey.startsWith(dataKey)
+}
 
 
 const sliderProps = computed(() => ({
@@ -33,18 +41,16 @@ if (props.config.key.key === '') {
 
 if (props.config.data) {
   for (const [dataKey, value] of Object.entries(props.config.data)) {
-    if (crossSellKey.caseInsensitive && keyMatcher(crossSellKey.key.toLowerCase(), dataKey.toLowerCase())) {
+    if (crossSellKey.caseInsensitive && keyMatcher(crossSellKey.key.toLowerCase(), dataKey.toLowerCase(), crossSellKey.regex)) {
       items = value
       break
     }
-    if (keyMatcher(crossSellKey.key, dataKey)) {
+    if (keyMatcher(crossSellKey.key, dataKey, crossSellKey.regex)) {
       items = value
       break
     }
   }
 }
-
-
 </script>
 
 <template>
@@ -81,12 +87,8 @@ if (props.config.data) {
         </template>
       </template>
     </Slider>
-    <div
-      v-else
-      class="flex flex-row"
-      :class="{ 'flex-wrap': !config.scroll, 'overflow-x-auto': config.scroll }"
-      :style="{ 'column-gap': config.columnGap }"
-    >
+    <div v-else class="flex flex-row" :class="{ 'flex-wrap': !config.scroll, 'overflow-x-auto': config.scroll }"
+      :style="{ 'column-gap': config.columnGap }">
       <template v-for="item in items">
         <a v-if="item.link" :href="item.link" :key="item.link" :style="config.itemStyle">
           <img v-if="item.image" :style="config.imageStyle" :src="item.image">
