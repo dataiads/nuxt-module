035e94e1aa9044ddb92965ebcbff18c9a32da9fa
YohanVirassamy <virassamy.yohan@gmail.com>
2024-02-28 14:47:48 +0100

    add new functions on the producthook

--- plugins/05.hooks.ts
+++ plugins/05.hooks.ts
@@ -30,3 +30,54 @@ export default defineNuxtPlugin((nuxtApp) => {
     }
   })
 })
+
+// PRODUCTHOOK FUNCTIONS
+// Get the device of user
+const getDeviceType = () => {
+  const ua = navigator.userAgent;
+  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+    return "tablet";
+  }
+  if (/Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(ua)
+  ) {
+    return "mobile"
+  }
+  return "desktop"
+}
+
+// Get the current price
+const getCurrentPrice = (price: string, salePrice: string) => {
+  if (salePrice !== null || salePrice !== undefined) {
+    return parseFloat(salePrice)
+  }
+  else {
+    return parseFloat(price)
+  }
+}
+
+// Get the browser of user
+const getBrowserType = () => {
+  var test = regexp => {
+    return regexp.test(navigator.userAgent);
+  };
+
+  if (test(/opr\//i) || !!window.opr) {
+    return 'opera';
+  } else if (test(/edg/i)) {
+    return 'microsoft edge';
+  } else if (test(/chrome|chromium|crios/i)) {
+    return 'google chrome';
+  } else if (test(/firefox|fxios/i)) {
+    return 'firefox';
+  } else if (test(/safari/i)) {
+    return 'safari';
+  } else if (test(/trident/i)) {
+    return 'internet explorer';
+  } else if (test(/ucbrowser/i)) {
+    return 'uc browser';
+  } else if (test(/samsungbrowser/i)) {
+    return 'samsung browser';
+  } else {
+    return 'unknown browser';
+  }
+}
