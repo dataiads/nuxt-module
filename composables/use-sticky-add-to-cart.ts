2018c6aeea7638bc26d1923b31b378ca8f548b6a
smesle <steve.demeulemeester@efrei.net>
2024-03-01 11:40:56 +0100

    rename composable add to cart

--- composables/use-sticky-add-to-cart.ts
+++ composables/use-sticky-add-to-cart.ts
@@ -1,6 +1,6 @@
 import type { MaybeComputedElementRef, MaybeElement } from '@vueuse/core'
 
-export const useAddToCart = (elements: MaybeComputedElementRef | MaybeRefOrGetter<MaybeElement[]> | MaybeComputedElementRef[]) => {
+export const useStickyAddToCart = (elements: MaybeComputedElementRef | MaybeRefOrGetter<MaybeElement[]> | MaybeComputedElementRef[]) => {
     const display = useState('addToCart.display')
     useIntersectionObserver(
     elements,
