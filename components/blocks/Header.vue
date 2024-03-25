49e26eb7a3df1b9b2d8e1bfd62db7e6adf1b0757
smesle <steve.demeulemeester@efrei.net>
2024-03-25 09:09:21 +0100

    header: work with popover and classic

--- components/blocks/Header.vue
+++ components/blocks/Header.vue
@@ -26,9 +26,9 @@ const mirroredDomain = useMirroredDomain()
 
 const popoverBtn = ref()
 const timeOut = ref()
-const handleEnter = (isOpen: boolean, i: number) => {
+const handleEnter = (open: boolean, i: number) => {
   clearTimeout(timeOut.value)
-  popoverBtn.value[i].$el.click()
+  !open && popoverBtn.value[i].$el.click()
 }
 
 const handleLeave = (i: number) => {
@@ -50,16 +50,16 @@ const handleLeave = (i: number) => {
     </div>
     <ul :class="ui.center">
       <slot name="center">
-        <ul v-for="(menu, i) in headerMenu" class="relative">
-          <Popover v-slot="{ open }" class="relative" @mouseleave="handleLeave(i)">
-            <PopoverButton ref="popoverBtn" @mouseover="handleEnter(open, i)">
-              <slot name="item" :item="menu" :index="i">
-                <a :href="menu.href" :class="ui.item" :style="{ color: menu.color }" v-html="menu.text" />
-              </slot>
-            </PopoverButton>
-            <PopoverPanel v-if="menu.children && menu.children.length" :class="ui.panel">
-              <slot name="children" :children="menu.children">
-                <div v-for="(child, i) in menu.children">
+              <ul v-for="(menu, i) in headerMenu" :key="'menu' + menu.text" class="relative">
+                <Popover v-slot="{ open }" class="relative" @mouseleave="menu.children ? handleLeave(i) : null">
+                  <component :is="menu.children ? PopoverButton : 'button'" ref="popoverBtn" @mouseover="menu.children ? handleEnter(open, i) : null">
+                  <slot name="item" :item="menu" :index="i">
+                    <a :href="menu.href" :class="ui.item" :style="{ color: menu.color }" v-html="menu.text" />
+                  </slot>
+                  </component>
+              <PopoverPanel v-if="menu.children && menu.children.length" :class="ui.panel">
+                <slot name="children" :children="menu.children">
+                    <div v-for="child in menu.children">
                   <a
                     :href="child.href"
                     :class="ui.item"
