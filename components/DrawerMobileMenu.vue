1dbb210e1376edc925c139506b5ae85d935e3480
smesle <steve.demeulemeester@efrei.net>
2024-03-29 19:24:49 +0100

    fix(DrawerMobileMenu): scroll when many items * Refactor mobile menu panel to support scrolling for improved user experience
    * The changes to the mobile menu panel enable overflow-y scrolling, enhancing user experience on smaller screens. Additionally, trailing icons are now correctly positioned.

--- components/DrawerMobileMenu.vue
+++ components/DrawerMobileMenu.vue
@@ -22,7 +22,7 @@ const isOpen = computed({
 })
 const defaultUI = {
   overlay: 'fixed inset-0 bg-black/30',
-  panel: 'w-full max-w-sm bg-white',
+  panel: 'w-full max-w-sm bg-white overflow-y-auto',
   buttonClose: 'flex flex-col ml-auto p-4',
   wrapper: 'flex flex-col divide-y border-t border-grey1/20',
   base: 'flex p-4 items-center',
@@ -33,8 +33,18 @@ const { ui } = useUI(defaultUI, toRefs(props.ui))
 
 <template>
   <TransitionRoot appear :show="isOpen" as="template">
-    <HDialog :open="isOpen" class="lg:hidden relative z-50" @close="setIsOpen">
-      <div :class="ui.overlay" aria-hidden="true" />
+    <HDialog as="div" class="relative z-10" @close="isOpen = false">
+      <TransitionChild
+        as="template"
+        enter="duration-300 ease-out"
+        enter-from="opacity-0"
+        enter-to="opacity-100"
+        leave="duration-200 ease-in"
+        leave-from="opacity-100"
+        leave-to="opacity-0"
+      >
+        <div :class="ui.overlay" />
+      </TransitionChild>
 
       <div class="fixed inset-0 flex w-3/4">
         <TransitionChild
@@ -58,6 +68,7 @@ const { ui } = useUI(defaultUI, toRefs(props.ui))
                   width="16"
                   height="16"
                 >
+                  
                   <path
                     d="M16 14.586L28.293 2.293a1 1 0 011.414 1.414L17.414 16l12.293 12.293a1 1 0 01-1.414 1.414L16 17.414 3.707 29.707a1 1 0 11-1.414-1.414L14.586 16 2.293 3.707a1 1 0 011.414-1.414L16 14.586z"
                     fill="#1A2A34"
@@ -66,24 +77,24 @@ const { ui } = useUI(defaultUI, toRefs(props.ui))
               </slot>
             </button>
             <ul :class="ui.wrapper">
-              <li v-for="(item, itemIndex) in headerMenu" :key="itemIndex" :class="ui.base">
-                <a :href="item.href" :style="{ color: item.color }">
+              <li v-for="(item, itemIndex) in headerMenu" :key="itemIndex">
+                <a :href="item.href" :style="{ color: item.color }" :class="ui.base">
                   {{ item.text }}
+                  <div :class="ui.trailingIcon">
+                    <slot name="trailingIcon">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="auto" viewBox="0 0 24 24">
+                        <path
+                          fill="none"
+                          stroke="#888888"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="1.5"
+                          d="m8.25 4.5l7.5 7.5l-7.5 7.5"
+                        />
+                      </svg>
+                    </slot>
+                  </div>
                 </a>
-                <div :class="ui.trailingIcon">
-                  <slot name="trailingIcon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="auto" height="auto" viewBox="0 0 24 24">
-                      <path
-                        fill="none"
-                        stroke="#888888"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="1.5"
-                        d="m8.25 4.5l7.5 7.5l-7.5 7.5"
-                      />
-                    </svg>
-                  </slot>
-                </div>
               </li>
             </ul>
           </DialogPanel>
