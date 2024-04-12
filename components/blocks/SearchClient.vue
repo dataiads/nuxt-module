3c57bc1f3f704930e8199df4a7ccd7ae4a49ca5e
smesle <steve.demeulemeester@efrei.net>
2024-04-12 14:33:39 +0200

    feat(SearchClient): new component

---
+++ components/blocks/SearchClient.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{
+  class?: string;
+}>()
+</script>
+
+<template>
+  <div class="w-full [&>form]:w-full">
+    <Search :redirect-url="$t('redirectSearchUrl')" :redirect-search-param="$t('redirectSearchParam')" :lpo-search-reccomendations="false">
+      <template
+        #default="{ value, input, loading }"
+      >
+        <slot 
+          :value="value"
+          :input="input"
+          :loading="loading"
+        >
+          <input
+            :placeholder="$t('searchPlaceholder')"
+            :value="value"
+            :class="class"
+            @input="input"
+          >
+        </slot>
+      </template>
+    </Search>
+  </div>
+</template>
