a4b44a8b5105ad8776d599e394457fafe559e34e
matthieudubo <matthieu.dubo40@gmail.com>
2024-06-06 11:13:37 +0200

    wip

---
+++ content-app/components/content/Docs/DocsFilterRating.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+const config = {
+  'title': 'Note',
+  'elements': [
+    {
+      'component': 'rating',
+      'props': {
+        'criteria': 'extraDataCustomAttributeNumeric.review-value',
+        'operator': 'RANGE',
+        'displayCount': true,
+        'useTranslation': false,
+        'prechecked': false,
+        'searchable': false,
+        'searchPlaceholder': 'Search...',
+        'min': '0',
+        'max': '10000',
+        'minPlaceholder': 'Min',
+        'maxPlaceholder': 'Max',
+        'label': '',
+        'value': '',
+        'value-criteria': '',
+        'step': 5,
+        'trailling': '€',
+        'placeholder': '',
+        'autoMinMax': false,
+        'icon': '',
+        'type': 'text',
+        'ariaLabel': '',
+        'valuesFilter': '',
+        'subTitle': '',
+        'subTitleStyle': {},
+        'starSize': '12px',
+        'starFullImg': 'https://api.iconify.design/heroicons:star-solid.svg',
+        'starEmptyImg': 'https://api.iconify.design/heroicons:star.svg',
+        'starsLabelTitle': '',
+        'moreLabelTitle': '',
+        'seeMore': false,
+        'limit': 5,
+        'seeMoreText': 'See more',
+        'seeLessText': 'See less'
+      }
+    }
+  ],
+  'style': {}
+}
+</script>
+
+<template>
+  <h1>bonjour</h1>
+</template>
---
+++ content-app/content/components/filter-rating.md
@@ -0,0 +1,9 @@
+# Filter Rating
+
+This component is internal to the custom layout
+
+::ClassTable{:data='[{"class":"filters-rating","desc":"Class used to apply CSS rules to the container of the checkbox and the stars"}]'}
+::
+
+::Showcase{name="DocsFilterRating"}
+::
