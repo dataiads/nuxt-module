5fcc4802256e87a23d574e419c13b36cf44d0ded
smesle <steve.demeulemeester@efrei.net>
2024-07-03 11:37:38 +0200

    reco-slider: simplify

--- composables/useRecoSlider.ts
+++ composables/useRecoSlider.ts
@@ -1,11 +1,11 @@
 import { createInjectionState } from '@vueuse/core'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import type { RecoSliderParams } from 'dataiads-nuxt-layer/types'
 
 export interface RecoSliderProps {
   config: RecoSliderParams,
   arrowPlacement: 'inside' | 'outside'
-  items: Product[][]
+  items: Ref<Product[][]>
 }
 
 const [useProvideRecoSlider, useInjectRecoSlider] = createInjectionState(
@@ -14,10 +14,20 @@ const [useProvideRecoSlider, useInjectRecoSlider] = createInjectionState(
     config,
     items
   }: RecoSliderProps) => {
+    const currentIndex = ref(1)
+    const slideInView = ref(1)
+
+    const getDisplayCount = computed<number>(() => 
+      Math.min(currentIndex.value * slideInView.value, items?.value?.length)
+    )
+
     return {
       arrowPlacement,
       config,
-      items
+      items,
+      currentIndex,
+      getDisplayCount,
+      slideInView
     }
   }
 )
