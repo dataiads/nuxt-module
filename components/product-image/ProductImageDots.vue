cb9fb69c2a28d63ac17ca8eecfffb871fabe7661
gollbargg <g.hossinin@gmail.com>
2024-07-15 11:05:40 +0200

    changes after review

--- components/product-image/ProductImageDots.vue
+++ components/product-image/ProductImageDots.vue
@@ -6,10 +6,21 @@ const { images, setIndex, index } = useProductImage()
     <div class="absolute bottom-4 left-4 flex gap-2 dots-container">
         <button v-for="(_, i) in images" :key="'dot_' + index"
             class="basis-full w-2 h-2 rounded-full cursor-pointer dots-btn" :class="{
-                'bg-[#E9E5E5]': index !== i,
-                'bg-[#565353] hover:[#565353]': index === i,
+                'dots-btn': index !== i,
+                'dots-btn-current': index === i,
             }" @click.prevent="setIndex(i)" />
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+
+.dots-btn {
+    @apply bg-secondary;
+}
+
+.dots-btn-current {
+    @apply bg-primary;
+}
+
+</style>
