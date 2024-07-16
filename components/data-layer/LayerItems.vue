aaa28939293097dc45f4129743958d5e19395413
gollbargg <g.hossinin@gmail.com>
2024-07-16 16:16:43 +0200

    Data layer refait

---
+++ components/data-layer/LayerItems.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { cn } from '@/lib/utils'
+const props = defineProps<{
+  class?: string;
+}>()
+
+const { items, config } = useLayer()
+console.log('items', items)
+</script>
+
+<template>
+  <CarouselItem
+    v-for="(item, index) in items"
+    :key="'layeritem_' + index"
+    :class="cn('', props.class)"
+    :style="{ paddingLeft: config.columnGap }"
+  >
+    <slot :item="item" />
+  </CarouselItem>
+</template>
