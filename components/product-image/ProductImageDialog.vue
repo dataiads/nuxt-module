e102c6ed736c5ba2bebdc7e745fe9998fa3c9694
smesle <steve.demeulemeester@efrei.net>
2024-07-22 08:19:06 +0200

    ProductImageDialog: use correct dialog

--- components/product-image/ProductImageDialog.vue
+++ components/product-image/ProductImageDialog.vue
@@ -1,22 +1,18 @@
 <script setup lang="ts">
-import { cn } from '~/lib/utils'
-const props = defineProps<{
-  class?: string
-}>()
+import SheetContent from '../ui/sheet/SheetContent.vue'
 const { dialog, index } = useProductImage()
 </script>
 
 <template>
-  <Dialog v-model:open="dialog">
-    <DialogContent :class="cn('lg:py-12 px-0 h-screen w-full max-w-screen', props.class)">
-      <div class="flex justify-center mx-auto">
-        <ProductImageMain>
-          <template #main-image="{ src }">
-            <img :src="src" class="w-full max-h-screen object-contain">
-          </template>
-          <ProductImageActions />
-        </ProductImageMain>
-      </div>
-    </DialogContent>
-  </Dialog>
+  <Sheet v-model:open="dialog">
+    <SheetContent side="full">
+      <ProductImageMain>
+        <template #main-image="{ src }">
+          <img :src="src" class="w-full max-h-screen object-contain">
+        </template>
+        <slot />
+        <ProductImageActions />
+      </ProductImageMain>
+    </SheetContent>
+  </Sheet>
 </template>
