ffbeb919287b891b672b07d74d6eeaea73d45ca7
sMesle <steve.demeulemeester@efrei.net>
2024-08-06 17:16:46 +0200

    reco slider: add class for different btns

--- components/reco-slider/RecoSliderNext.vue
+++ components/reco-slider/RecoSliderNext.vue
@@ -14,15 +14,21 @@ const displayNext = computed(() => canScrollNext.value || props.displayDisable)
 </script>
 
 <template>
-  <CarouselNext v-if="config.nextButton && displayNext" :class="cn('', arrowPlacement === 'inside' ? 'right-4 z-10' : 'relative left-0 border-none translate-y-0', 'reco-slider-btn', props.class)" :style="config.buttonStyle">
+  <CarouselNext
+    v-if="config.nextButton && displayNext"
+    :class="cn('', arrowPlacement === 'inside' ? 'right-4 z-10' : 'relative left-0 border-none translate-y-0', 'reco-slider-btn reco-slider-btn-next', props.class)"
+    :style="config.buttonStyle"
+  >
     <img v-if="config.nextButton" :src="config.nextButton">
-    <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path
-      fill="none"
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="m8.25 4.5l7.5 7.5l-7.5 7.5"
-    /></svg>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+        d="m8.25 4.5l7.5 7.5l-7.5 7.5"
+      />
+    </svg>
   </CarouselNext>
 </template>
--- components/reco-slider/RecoSliderPrev.vue
+++ components/reco-slider/RecoSliderPrev.vue
@@ -13,15 +13,21 @@ const displayPrev = computed(() => canScrollPrev.value || props.displayDisable)
 </script>
 
 <template>
-  <CarouselPrevious v-if="config.previousButton && displayPrev" :class="cn('', arrowPlacement === 'inside' ? 'left-4 z-10' : 'relative left-0 border-none translate-y-0', 'reco-slider-btn', props.class)" :style="config.buttonStyle">
+  <CarouselPrevious
+    v-if="config.previousButton && displayPrev"
+    :class="cn('', arrowPlacement === 'inside' ? 'left-4 z-10' : 'relative left-0 border-none translate-y-0', 'reco-slider-btn reco-slider-btn-prev', props.class)"
+    :style="config.buttonStyle"
+  >
     <img v-if="config.previousButton" :src="config.previousButton">
-    <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path
-      fill="none"
-      stroke="currentColor"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="1.5"
-      d="M15.75 19.5L8.25 12l7.5-7.5"
-    /></svg>
+    <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+      <path
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="1.5"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
+      />
+    </svg>
   </CarouselPrevious>
 </template>
